0cc41530cc717e3d9c2da5f67277ac1c7095152c
warolv <warolv@gmail.com>
2019-03-21 16:31:33 +0200

    Change service_client indentation

--- service_client.rb
+++ service_client.rb
@@ -5,20 +5,20 @@ require 'nokogiri'
 class ServiceClient
   def self.get_status(url, type = 'json')
     begin
-			response = RestClient.get(url)
-			
+      response = RestClient.get(url)
+      
       if response.code == 200
-				if type == 'json'
-				  ['good','ok'].include?(JSON.parse(response)['status']['overall'].to_s.downcase) ? 'up' : 'down'
-				else # xml
-	        Nokogiri::XML(response).xpath('//status').inner_text.to_s.downcase == 'good' ? 'up' : 'down'
-				end
+        if type == 'json'
+          ['good','ok'].include?(JSON.parse(response)['status']['overall'].to_s.downcase) ? 'up' : 'down'
+        else # xml
+          Nokogiri::XML(response).xpath('//status').inner_text.to_s.downcase == 'good' ? 'up' : 'down'
+        end
       else
         'unknown'
-			end
-		rescue # in case of timeout or some unexpected problem with service
+      end
+    rescue # in case of timeout or some unexpected problem with service
       'unknown'
-		end
+    end
   end
 end
 
