979f01001708ddf1e8b22dbb423afcad48930788
warolv <warolv@gmail.com>
2019-03-21 14:13:49 +0200

    Align Service Client indentation

--- service_client.rb
+++ service_client.rb
@@ -15,7 +15,7 @@ class ServiceClient
       else
         'unknown'
 			end
-		rescue # in case of timeout or some unexpected problem with service
+		rescue # in case of timeout or some unexpected problem with the service
       'unknown'
 		end
   end
